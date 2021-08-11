3ab6df1f3a1b55b9b8e3be23f698f41e3a065ed0
Kyle Kotowick <kyle@kotowick.ca>
2021-08-11 11:31:21 -0400

    Formatting

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "stdout" {
   description = "The stdout output of the shell command."
-  value = local.result.stdout
+  value       = local.result.stdout
 }
 
 output "stderr" {
   description = "The stderr output of the shell command."
-  value = local.result.stderr
+  value       = local.result.stderr
 }
 
 output "exitstatus" {
   description = "The exit status code of the shell command."
-  value = local.result.exitstatus
+  value       = local.result.exitstatus
 }
