d4538d01c853f78f34002f12e4b492de3a4dd7d9
Kyle Kotowick <kotowick@invictonlabs.com>
2021-10-03 10:38:35 -0400

    Fix formatting

--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ data "aws_lambda_invocation" "shell" {
   ]
   function_name = local.wait_for_apply == null ? var.lambda_shell_module.invicton_labs_lambda_shell_arn : var.lambda_shell_module.invicton_labs_lambda_shell_arn
   input = jsonencode({
-    interpreter = var.interpreter
-    command = var.command
+    interpreter   = var.interpreter
+    command       = var.command
     fail_on_error = var.fail_on_error
   })
 }
